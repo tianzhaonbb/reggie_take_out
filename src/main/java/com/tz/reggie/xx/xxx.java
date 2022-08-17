5264f84cb68966c130c6426e0ca09fc3e41b2b70
tianzhao <2501267180@qq.com>
2022-08-18 01:14:59 +0800

    niubi

--- src/main/java/com/tz/reggie/xx/xxx.java
+++ src/main/java/com/tz/reggie/xx/xxx.java
@@ -3,5 +3,9 @@ package com.tz.reggie.xx;
 public class xxx {
     public static void main(String[] args) {
         System.out.println(112);
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
     }
 }
